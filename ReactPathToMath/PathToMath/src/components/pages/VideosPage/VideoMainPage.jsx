fbb7c2e484dd8064647f09f664dcc2583ea03354
AdanIb <Adan.Ibrahim@e.braude.ac.il>
2025-05-10 14:24:49 +0300

    Initial commit

--- ReactPathToMath/PathToMath/src/components/pages/VideosPage/VideoMainPage.jsx
+++ ReactPathToMath/PathToMath/src/components/pages/VideosPage/VideoMainPage.jsx
@@ -1,32 +1,49 @@
 import React from 'react';
-import background from '../../../assets/Images/Background/background3.png'; 
+import background from '../../../assets/Images/Background/back.png';
 
+import additionIcon from '../../../assets/Images/Math_icon/addition_purple.png';
+import subtractionIcon from '../../../assets/Images/Math_icon/minus.png';
+import multiplicationIcon from '../../../assets/Images/Math_icon/multi.png';
+import divisionIcon from '../../../assets/Images/Math_icon/division1.png';
 
 const VideoMainPage = () => {
   const subjects = [
-    { name: 'Addition' },
-    { name: 'Subtraction' },
-    { name: 'Multiplication' },
-    { name: 'Division' },
+    { name: 'Addition', icon: additionIcon },
+    { name: 'Subtraction', icon: subtractionIcon },
+    { name: 'Multiplication', icon: multiplicationIcon },
+    { name: 'Division', icon: divisionIcon },
   ];
 
   return (
+    
     <div
-      className="relative min-h-[80vh] bg-blue-100 px-4 pt-10 pb-24 flex flex-col items-center justify-start overflow-hidden"
-      style={{ backgroundImage: `url(${background})` }}
+      className="relative min-h-[100vh] w-full flex flex-col items-center justify-start pt-12 pb-24 px-4 overflow-hidden"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
     >
-      <h1 className="text-4xl md:text-5xl font-bold text-white mb-10 drop-shadow-lg">
-        Choose a Topic
-      </h1>
+     <h1 className="text-4xl md:text-5xl font-bold text-[#3B2F2F] mb-10 drop-shadow-sm">
+        Let's Learn Math!
+    </h1>
 
-      <div className="flex flex-wrap justify-center gap-6">
+      <div className="flex flex-wrap justify-center gap-10 z-10">
         {subjects.map((subject, index) => (
-          <button
+          <div
             key={index}
-            className="bg-yellow-300 hover:bg-yellow-400 text-lg font-semibold text-gray-800 px-8 py-4 rounded-2xl shadow-md transition-transform duration-200 hover:scale-105"
+            className="p-10 w-70 h-70 flex flex-col items-center justify-center transition-transform duration-200 hover:scale-105"
           >
-            {subject.name}
-          </button>
+            <img
+              src={subject.icon}
+              alt={subject.name}
+              className="w-50 h-50 mb-4 object-contain aspect-square drop-shadow-md"
+            />
+            <span className="text-center text-lg font-semibold text-gray-800">
+              {subject.name}
+            </span>
+          </div>
         ))}
       </div>
     </div>
