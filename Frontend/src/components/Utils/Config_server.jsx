27496f401ca4aab9c16a45054143fe00999728fb
YardenNahum <yardennahum2@gmail.com>
2025-06-13 14:09:26 +0300

    domain fix

--- Frontend/src/components/Utils/Config_server.jsx
+++ Frontend/src/components/Utils/Config_server.jsx
@@ -1,6 +1,6 @@
 // config.js
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://path-to-math.vercel.app/'
+    ? 'https://path-to-math-qw451nz4k-yardennahums-projects.vercel.app/'
     : 'http://localhost:5000';
 export default BASE_URL;
--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ if (!mongoose.connection.readyState) {
 // Middlewares
 app.use(cors({
   origin: [  'http://localhost:5173',
-'http://localhost:3000', 'https://path-to-math.vercel.app/'],
+'http://localhost:3000', 'https://path-to-math-qw451nz4k-yardennahums-projects.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
