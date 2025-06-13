331a7be696931c71c0620cd44ab767e526b44e2e
YardenNahum <yardennahum2@gmail.com>
2025-06-13 15:05:13 +0300

    another fix

--- Frontend/src/components/Main/ProfilePage/ProfilePage.jsx
+++ Frontend/src/components/Main/ProfilePage/ProfilePage.jsx
@@ -145,7 +145,7 @@ const ProfilePage = () => {
           ? "border-blue-200"
           : "border-transparent";
 
-      const avatarSrc = `../../../assets/Images/Avatars/${avatarEndSrc}.png`;
+      const avatarSrc = `./Frontend/src/assets/Images/Avatars/${avatarEndSrc}.png`;
 
       const handleAvatarClick = () => {
         setAvatar(avatarSrc);
