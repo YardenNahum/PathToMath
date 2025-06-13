352fec3a55409810480c991d8910c36246df3d4c
YardenNahum <yardennahum2@gmail.com>
2025-06-13 14:15:20 +0300

    domain fix

--- Frontend/src/components/Utils/Config_server.jsx
+++ Frontend/src/components/Utils/Config_server.jsx
@@ -1,6 +1,6 @@
 // config.js
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://path-to-math.vercel.app'
+    ? 'https://path-to-math-ohap16tdm-yardennahums-projects.vercel.app'
     : 'http://localhost:5000';
 export default BASE_URL;
