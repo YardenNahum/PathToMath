1b06a0480fa6779b949c766e070933c8fe826b16
guyshargo <shargo501@gmail.com>
2025-07-08 12:33:37 +0300

    ahhh2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,14 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'dist', // output directory for the build
+    emptyOutDir: true, // clear the output directory before building
+  },
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
   },
   proxy: {
     '/api': { // proxy api requests to the server
-      target: 'http://localhost:3000/', //  target server
-      changeOrigin: true,
+    target: 'http://localhost:3000/', //  target server
+    changeOrigin: true,
     },
   },
 
